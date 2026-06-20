15b5033d17af7b993f0e6372c8b40e90f5fdb079
UnSmartest <kwidhi@gmail.com>
2026-06-20 07:45:20 +0700

    Created constants.py
    Make sure to declare any constants here so that we don't have to go through the hassle of having magic numbers

---
+++ constants.py
@@ -0,0 +1,10 @@
+# Just to prevent declaring numbers in one file and then having to get that number in another file
+# Or uh magic numbers as they call it
+WINDOW_WIDTH = 768
+WINDOW_HEIGHT = 512
+TILE_SIZE = 32
+
+CHAT_TILE_WIDTH = 8
+CHAT_TILE_HEIGHT = 16
+MAZE_TILE_WIDTH = 16
+MAZE_TILE_HEIGHT = 16
